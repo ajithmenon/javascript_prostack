353000eef4c832338ea3c4e0b6aa5157ba58fe1e
ajithmenon <ajithachus20@gmail.com>
2025-02-18 10:56:44 +0530

    Class Feb 05

---
+++ Class-01_002/dt.js
@@ -0,0 +1,14 @@
+//create variable
+var eid = 101;
+var ename ="Rahul"
+
+// Read variable
+console.log(eid)
+console.log(ename);
+
+// update variable
+eid = ["wayanad", "New Delhi"]
+console.log(eid);
+
+eid = true;
+console.log(eid)
