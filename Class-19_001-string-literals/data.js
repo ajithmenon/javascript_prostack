8417fa2ee53a6eb677b7e82c6ccd810fcf81d845
ajithmenon <ajithachus20@gmail.com>
2025-03-07 10:10:44 +0530

    Class March 07

---
+++ Class-19_001-string-literals/data.js
@@ -0,0 +1,8 @@
+
+eid = 101
+ename =  "Ajith"
+
+console.log(` Employee id :`, eid,`  and Employee name : `,ename)
+
+
+console.log(` Employee id : ${eid} and Employee name : ${ename}`)            // print using string literals
